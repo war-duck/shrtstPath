4a8768edfb2d3e9fe6bc01cb5ea1566a0dcd8489
war-duck <marcin.l.wardak@gmail.com>
2022-12-14 09:19:30 +0100

    update 1.

--- create_lab.cpp
+++ create_lab.cpp
@@ -1,7 +1,7 @@
 #include <cstdio>
 #include <iostream>
 #include <cstdlib>
-#define LAB_SIZE 8
+#define LAB_SIZE 4
 
 void print_arr (double arr[LAB_SIZE][LAB_SIZE])
 {
@@ -10,6 +10,18 @@ void print_arr (double arr[LAB_SIZE][LAB_SIZE])
         for (int j = 0; j < LAB_SIZE; ++j)
             printf ("%6.4f\t", arr[i][j]);
         printf ("\n");
+    }
+}
+
+void print_lab (double lab[LAB_SIZE*LAB_SIZE][LAB_SIZE*LAB_SIZE])
+{
+    for (int i = 0; i < LAB_SIZE-1; ++i)
+    {
+        for (int j = 0; j < LAB_SIZE-1; ++j)
+        {
+            printf("%d\t%c\t", i*LAB_SIZE+j, lab[i*LAB_SIZE+j][i*LAB_SIZE+j+1]?'=':'\0');
+        }
+        std::cout << (i+1)*LAB_SIZE-1 << '\n';
     }
 }
 
@@ -27,10 +39,10 @@ int main()
     for (int i = 0; i < LAB_SIZE*LAB_SIZE; ++i)
         for (int j = 0; j < LAB_SIZE*LAB_SIZE; ++j)
             lab[i][j] = 0.0;
-    short options[5], crnt_dir; // 1/2/3/4 - Prawo/Dół/Lewo/Góra   
-    short connection_exists;     
+    short options[5]; // 1/2/3/4 - Prawo/Dół/Lewo/Góra   
+    short connection_exists;
+    signed int diff;
     for (int i = 0; i < LAB_SIZE; ++i)
-    {
         for (int j = 0; j < LAB_SIZE; ++j)
         {
             connection_exists = 0;
@@ -45,15 +57,19 @@ int main()
             std::cout << i*LAB_SIZE+j << ": ";
             for (int k = 0; k < options[4]; ++k) // dla każdego dostępnego kierunku decyduję, czy istnieje połączenie
             {
-                if (my_rand % 16 < 9) // przyjalem prawdopodobienstwo polaczenia 9/16
+                // diff = (options[k]/3>=1?-1:1)*(options[k]%2?LAB_SIZE:1);
+                // std::cout << "diff: " << diff << '\n';
+                if (my_rand % 16 < 3) // przyjalem prawdopodobienstwo polaczenia 9/16
                 {
                     switch (options[k])
                     {
-                        case 1: lab[i*LAB_SIZE+j][i*LAB_SIZE+j+1] = (arr[i][j]+arr[i][j+1])/2.0; std::cout << 'P'; break;
-                        case 2: lab[i*LAB_SIZE+j][(i+1)*LAB_SIZE+j] = (arr[i][j]+arr[i+1][j])/2.0; std::cout << 'D'; break;
-                        case 3: lab[i*LAB_SIZE+j][i*LAB_SIZE+j-1] = (arr[i][j]+arr[i][j-1])/2.0; std::cout << 'L'; break;
-                        case 4: lab[i*LAB_SIZE+j][(i-1)*LAB_SIZE+j] = (arr[i][j]+arr[i-1][j])/2.0; std::cout << 'G'; break;
+                        case 1: lab[i*LAB_SIZE+j][i*LAB_SIZE+j+1]   = lab[i*LAB_SIZE+j+1][i*LAB_SIZE+j]   = (arr[i][j]+arr[i][j+1])/2.0; std::cout << 'P'; break;
+                        case 2: lab[i*LAB_SIZE+j][(i+1)*LAB_SIZE+j] = lab[(i+1)*LAB_SIZE+j][i*LAB_SIZE+j] = (arr[i][j]+arr[i+1][j])/2.0; std::cout << 'D'; break;
+                        case 3: lab[i*LAB_SIZE+j][i*LAB_SIZE+j-1]   = lab[i*LAB_SIZE+j-1][i*LAB_SIZE+j]   = (arr[i][j]+arr[i][j-1])/2.0; std::cout << 'L'; break;
+                        case 4: lab[i*LAB_SIZE+j][(i-1)*LAB_SIZE+j] = lab[(i-1)*LAB_SIZE+j][i*LAB_SIZE+j] = (arr[i][j]+arr[i-1][j])/2.0; std::cout << 'G'; break;
                     }
+                    //lab[i*LAB_SIZE+j][i*LAB_SIZE+j+diff] = lab[i*LAB_SIZE+j+diff][i*LAB_SIZE+j] = (arr[i][j] + (options[k]%2?arr[i+diff/LAB_SIZE][j]:arr[i][diff])) / 2;
+                    std::cout << options[k];
                     connection_exists = 1;
                 }
                 my_rand >>=4;
@@ -61,13 +77,13 @@ int main()
             if (! connection_exists)
                 switch (options[my_rand%options[4]+1])
                 {
-                    case 1: lab[i*LAB_SIZE+j][i*LAB_SIZE+j+1] = (arr[i][j]+arr[i][j+1])/2.0; std::cout << "\t dolosowano P"; break;
-                    case 2: lab[i*LAB_SIZE+j][(i+1)*LAB_SIZE+j] = (arr[i][j]+arr[i+1][j])/2.0; std::cout << "\t dolosowano D"; break;
-                    case 3: lab[i*LAB_SIZE+j][i*LAB_SIZE+j-1] = (arr[i][j]+arr[i][j-1])/2.0; std::cout << "\t dolosowano L"; break;
-                    case 4: lab[i*LAB_SIZE+j][(i-1)*LAB_SIZE+j] = (arr[i][j]+arr[i-1][j])/2.0; std::cout << "\t dolosowano G"; break;
+                    case 1: lab[i*LAB_SIZE+j][i*LAB_SIZE+j+1]   = lab[i*LAB_SIZE+j+1][i*LAB_SIZE+j]   = (arr[i][j]+arr[i][j+1])/2.0; std::cout << "\t dolosowano P"; break;
+                    case 2: lab[i*LAB_SIZE+j][(i+1)*LAB_SIZE+j] = lab[(i+1)*LAB_SIZE+j][i*LAB_SIZE+j] = (arr[i][j]+arr[i+1][j])/2.0; std::cout << "\t dolosowano D"; break;
+                    case 3: lab[i*LAB_SIZE+j][i*LAB_SIZE+j-1]   = lab[i*LAB_SIZE+j-1][i*LAB_SIZE+j]   = (arr[i][j]+arr[i][j-1])/2.0; std::cout << "\t dolosowano L"; break;
+                    case 4: lab[i*LAB_SIZE+j][(i-1)*LAB_SIZE+j] = lab[(i-1)*LAB_SIZE+j][i*LAB_SIZE+j] = (arr[i][j]+arr[i-1][j])/2.0; std::cout << "\t dolosowano G"; break;
                 }
             std::cout << "\n\n";
         }
-    }
+    print_lab(lab);
     return 0;
 }
