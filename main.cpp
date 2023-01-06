097d9656a2a97c860e28ed22566e13413a8773e1
war-duck <marcin.l.wardak@gmail.com>
2023-01-06 19:53:54 +0100

    ostatnie poprawki - CLEAN_LAB

--- main.cpp
+++ main.cpp
@@ -4,15 +4,6 @@ int main()
 {
     system("CLS");
     result lab_data = create_paths();
-    //std::cout << lab_data->lab_str;
-    // for (int i = 0; i < LAB_SIZE*LAB_SIZE; ++i)
-    // {
-        
-    //     for (int j = 0; j < LAB_SIZE*LAB_SIZE; ++j)
-    //         std::cout << (*(lab_data->lab+i))[j] << '\t';
-    //     std::cout << '\n';
-    // }
-    //solve(LAB_SIZE*LAB_SIZE, lab_data->start, lab_data->dest, lab_data);
     std::cout << "szukam...\n";
 #ifndef SHOW_ARR
     add_path_anim(&lab_data->lab_str, solve(LAB_SIZE*LAB_SIZE, lab_data->start, lab_data->dest, lab_data));
--- solver.cpp
+++ solver.cpp
@@ -1,57 +1,5 @@
 #include "solver.hpp"
 using namespace std;
-// struct
-// {
-// 	vector< vector<int> > paths;
-// 	vector<double> path_lengths;
-// 	set<int> start_neigh;
-// 	set<int> end_neigh;
-// 	int path_depth;
-// 	int solution_no = -1;
-// } solutions;
-
-// pair<vector<int>, double> shrtstPath(int start, int dest, vector<vector<double>>& graph, set<int> visited)
-// {
-// 	if (graph[start][dest] != -1) // jest juz bezposrednie polaczenie z koncem
-// 	{
-// 		++solutions.solution_no;
-// 		solutions.paths.insert(solutions.paths.begin(), vector<int>());
-// 		solutions.path_depth = 1;
-// 		cout << start << "znaleziono \t";
-// 		cout << solutions.path_depth << '\t' << solutions.solution_no << '\n';
-// 		return pair<vector<int>, double>(vector<int>(1,dest), graph[start][dest]);
-// 	}
-// 	visited.insert(start); // do odwiedzonych wierzcholkow dodajemy obecny
-// 	pair<vector<int>, double> minResult (vector<int>(), double(~(unsigned int)0)), currentResult;
-// 	for (short opt = 0; opt < graph[start].size(); ++opt) // dla punktu startowego
-// 	{
-// 		if (graph[start][opt] != -1 && visited.count(opt) == false) // sprawdzamy dostepne polaczenia
-// 		{
-// 			//cout << string(visited.size(), '\t') << "Wywolywanie: " << opt << '\n';
-// 			currentResult = shrtstPath(opt, dest, graph, visited); // dla kazdego sasiada sprawdzamy jego sasiadow
-// 				// solutions.paths.insert(solutions.paths.end(), currentResult.first);
-// 				// solutions.path_lengths.insert(solutions.path_lengths.end(), currentResult.second);
-// 			//cout << string(visited.size(), '\t') << "Wynik: " << currentResult.second << endl;
-// 			if (currentResult.first.size() == solutions.path_depth && solutions.solution_no != -1)
-// 			{
-// 				cout << start << "\t\t";
-// 				cout << ++solutions.path_depth << '\t' << solutions.solution_no << '\n';
-// 				solutions.paths[solutions.solution_no].insert(solutions.paths[solutions.solution_no].begin() + solutions.path_depth -1, start);
-// 				cout << "inserted";
-// 			}
-// 			if (currentResult.second+graph[start][opt] < minResult.second) // jesli obecna dlugosc < dotychczasowa minimalna
-// 			{
-// 				minResult = currentResult;
-// 				minResult.second += graph[start][opt];
-// 				minResult.first.insert(minResult.first.begin(), opt);
-
-// 			}
-			
-
-// 		}
-// 	}
-// 	return minResult;
-// }
 
 pair<vector<int>, double> shrtstPath(int start, int dest, vector<vector<double>>& graph, set<int> visited)
 {
@@ -63,11 +11,7 @@ pair<vector<int>, double> shrtstPath(int start, int dest, vector<vector<double>>
 	{
 		if (graph[start][opt] != -1 && visited.count(opt) == false) // sprawdzamy dostepne polaczenia
 		{
-			//cout << string(visited.size(), '\t') << "Wywolywanie: " << opt << '\n';
 			currentResult = shrtstPath(opt, dest, graph, visited); // dla kazdego sasiada sprawdzamy jego sasiadow
-				// solutions.paths.insert(solutions.paths.end(), currentResult.first);
-				// solutions.path_lengths.insert(solutions.path_lengths.end(), currentResult.second);
-			//cout << string(visited.size(), '\t') << "Wynik: " << currentResult.second << endl;
 			if (currentResult.second+graph[start][opt] < minResult.second) // jesli obecna dlugosc < dotychczasowa minimalna
 			{
 				minResult = currentResult;
